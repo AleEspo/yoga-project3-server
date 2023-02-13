f1fd397658d13ec928ec1ad114de6410778f586f
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2023-02-13 19:37:48 +0100

    add ethereal test

--- config/transporter.config.js
+++ config/transporter.config.js
@@ -1,11 +1,20 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "hotmail",
+  // Ethereal test
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_ADDRESS_hotmail,
-    pass: process.env.EMAIL_PASSWORD,
-  },
+    user: process.env.ETHE_USER,
+    pass: process.env.ETHE_PASS,
+  }
+  // Original Code
+  // service: "hotmail",
+  // auth: {
+  //   user: process.env.EMAIL_ADDRESS_hotmail,
+  //   pass: process.env.EMAIL_PASSWORD,
+  // },
 });
 
 transporter.verify((error, success) => {

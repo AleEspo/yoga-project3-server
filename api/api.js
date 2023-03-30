7554cbd36befa5323e500efa3f81b0751421d2d6
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2023-03-30 15:14:18 -0300

    changed api.js process.env.NODE_ENV from production to prod (see cyclic environments

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = process.env.NODE_ENV === 'production' 
+const apiUrl = process.env.NODE_ENV === 'prod' 
   ? `https://yogahome.cyclic.app`
   : `http://localhost:${Number(process.env.PORT)}`;
 

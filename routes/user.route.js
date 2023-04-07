8f8d90071f29225cc94589b7e776b464ad899262
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2023-04-07 13:07:41 -0300

    test 2

--- routes/user.route.js
+++ routes/user.route.js
@@ -70,6 +70,9 @@ const sendVerificationEmail = async ({ _id, email }, res) => {
     // }; ?
     // const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
 
+    // const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
+
+
     const currentUrl = "https://yogahome.cyclic.app" ;
     const uniqueString = uuidv4() + _id;
 
